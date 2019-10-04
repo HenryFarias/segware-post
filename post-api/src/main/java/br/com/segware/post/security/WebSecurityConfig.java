2d5b2ad087a28dbad3d491e6daa49da0f403759d
HenryFarias <henrysjfarias@gmail.com>
2019-10-04 09:07:05 -0300

    retirando comentarios

--- post-api/src/main/java/br/com/segware/post/security/WebSecurityConfig.java
+++ post-api/src/main/java/br/com/segware/post/security/WebSecurityConfig.java
@@ -26,12 +26,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-//                .antMatchers("/post/**").hasRole("POST_GRANT_ALL")
-//                .antMatchers(HttpMethod.GET, "/post").hasAnyRole("POST_WRITE_READ", "POST_READ")
-//                .antMatchers(HttpMethod.POST,"/post").hasRole("POST_WRITE_READ")
-//                .antMatchers(HttpMethod.POST,"/post/{id}/add/upVote").hasAnyRole("POST_WRITE_READ", "POST_READ")
-//                .antMatchers(HttpMethod.POST,"/post/{id}/remove/upVote").hasAnyRole("POST_WRITE_READ", "POST_READ")
-//                .antMatchers("/user/**").hasRole("USER_GRANT_ALL")
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/swagger-ui.html").permitAll()
                 .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
